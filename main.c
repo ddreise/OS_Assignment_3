aeac0ff937c8745f017a1e40e7079b5f62087003
ddreise <ddreise@gmail.com>
2019-10-31 18:02:33 -0400

    Hexdumps working, moving to -d and -h

--- main.c
+++ main.c
@@ -14,42 +14,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_FILE_SZ 4294967296		// 4 gb
+#define MAX_FILE_SZ 4300000000		// ~4 gb
+#define MAX_RDM_FILE_SZ 160
 
 int main(int argc, char *argv[]) {
 	
 	FILE *fp;				// File handler (pointer)
 	char test[256];			// Variable for file name
 	unsigned long int sz;	// Variable for file size in bytes
-	char str[256];						// File string. Max 256 characters
+	char str[256];			// File string. Max 256 characters
 
-
-	strcpy(test, "test -a ");	// Terminal command to test of file exists
-	strcat(test, argv[1]);
+	sprintf(test, "test -a %s", argv[1]);	// Terminal command to test if file exists
 
 	// If the second command line argument is a file & it exists, open it
 	if ((argc == 2) & (system(test)==0)){	
 		
-		sprintf(str, "hexdump -Cn %ld %s", MAX_FILE_SZ, argv[1]);
+		sprintf(str, "hexdump -n %ld %s", MAX_FILE_SZ, argv[1]);
 		printf("%s\n", str);
-		//strcpy(str, "hexdump -Cn  160");		// Terminal command "hexdump"
-		//strcat(str, MAX_FILE_SZ);
-		//strcat(str, " ");
-		//strcat(str, argv[1]);				// concatenated with chosen file to hexdump
 
 		fp = fopen(argv[1], "r");			// open file
 
 		fseek(fp, 0L, SEEK_END);			// find end of the file
 		sz = ftell(fp);						// report how many bytes are in file
+		if (sz > MAX_FILE_SZ) sz = MAX_FILE_SZ;	// If file is bigger than 4GB, restrict amount to hexdump
 		rewind(fp);							// return file pointer to begining
 
 		// Indicate file opened
 		printf("\n\n********** %s file opened! **********\n\n", argv[1]);	
 
-		system(str);			// Send hexdump command to terminal
-		if(sz < 60){			// If file is over max file size (bytes), indicate End of File
-			printf("\n\nEND OF FILE\n\n");
-		}
+		system(str);					// Send hexdump command to terminal
+		
+		printf("\n\nEND OF FILE\n\n");
 		
 
 		fclose(fp);								// close file
@@ -58,32 +53,24 @@ int main(int argc, char *argv[]) {
 		printf("\n\n********** %s file closed! **********\n\n", argv[1]);	
 
 	}
-	else {
-
-		strcpy(str, "hexdump -Cn 160");		// Terminal command "hexdump"
-		strcat(str, argv[1]);				// concatenated with chosen file to hexdump
+	else if ((argc <= 1) | (system(test)!=0)) {
+		sprintf(str, "hexdump -n %d /dev/urandom", MAX_RDM_FILE_SZ);
 
 		// Indicate error if improper arguments used
-		fprintf(stderr, "ERROR: Too many arguments or file doesn't exist.\n");
-		fprintf(stderr, "Printing /dev/urandom instead\n");	
+		fprintf(stderr, "\nERROR: Too few arguments or file doesn't exist.\n");
+		fprintf(stderr, "Hexdumping /dev/urandom instead\n");	
 
 		fp = fopen("/dev/urandom", "r");		// Open /dev/urandom
 
-		fseek(fp, 0L, SEEK_END);			// find end of the file
-		sz = ftell(fp);						// report how many bytes are in file
-		rewind(fp);							// return file pointer to begining
-
-		printf("\n\n********** %s file opened! **********\n\n", argv[1]);	
+		printf("\n\n********** /dev/urandom file opened! **********\n\n");	
 
 		system(str);					// Send hexdump command to terminal
-		if(sz < MAX_FILE_SZ){					// If file is over max file size (bytes), indicate End of File
-			printf("\n\nEND OF FILE\n\n");
-		}
+		printf("\n\nEND OF FILE\n");
 		
 		fclose(fp);								// close file
 
 		// Indicate file closed
-		printf("\n\n********** %s file closed! **********\n\n", argv[1]);
+		printf("\n********** /dev/urandom file closed! **********\n\n");
 
 	}
 
